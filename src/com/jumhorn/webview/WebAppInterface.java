6fb0be4d90148b4e29ab2092c28148ce8bc04dc3
JumHorn <JumHorn@gmail.com>
2022-10-07 17:05:14 +0800

    show alert

--- src/com/jumhorn/webview/WebAppInterface.java
+++ src/com/jumhorn/webview/WebAppInterface.java
@@ -1,7 +1,9 @@
 package com.jumhorn.webview;
 
 import android.content.Context;
+import android.widget.Button;
 import android.widget.Toast;
+import android.app.AlertDialog;
 import android.webkit.JavascriptInterface;
 
 public class WebAppInterface {
@@ -16,5 +18,15 @@ public class WebAppInterface {
     @JavascriptInterface
     public void showToast(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+	//show alert
+	@JavascriptInterface
+    public void showAlert(String msg) {
+		new AlertDialog.Builder(mContext)
+		.setMessage(msg)
+		// A null listener allows the button to dismiss the dialog and take no further action.
+		.setNegativeButton(android.R.string.no, null)
+		.show();
     }
 }
