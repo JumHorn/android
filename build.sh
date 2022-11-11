d48363db81f3576d2e5fc4f915e102e3e9bfa356
JumHorn <JumHorn@gmail.com>
2022-11-11 12:48:41 +0000

    update build file for ubuntu

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # add environment path
-export PATH="/opt/android-sdk/cmdline-tools/latest/build-tools/28.0.3:$PATH"
+# export PATH="/opt/android-sdk/cmdline-tools/latest/build-tools/28.0.3:$PATH"
+android_jar="/usr/lib/android-sdk/platforms/android-23/"
 
 # generate the R.java file
 echo "##### generate the R.java file"
-aapt package -f -m -J ./src -M ./AndroidManifest.xml -S ./res -I /opt/android-sdk/platforms/android-31/android.jar
+aapt package -f -m -J ./src -M ./AndroidManifest.xml -S ./res -I ${android_jar}android.jar
 
 # compile the .java files
 echo "##### compile the .java files"
-javac -source 1.7 -target 1.7 -d obj -classpath ./src -bootclasspath /opt/android-sdk/platforms/android-31/android.jar src/com/jumhorn/webview/*.java src/com/jumhorn/textview/*.java
+javac -source 1.7 -target 1.7 -d obj -classpath ./src -bootclasspath ${android_jar}android.jar src/com/jumhorn/webview/*.java src/com/jumhorn/textview/*.java
 
 # translate .class to classes.dex
 echo "##### translate .class to classes.dex"
@@ -16,7 +17,7 @@ dx --dex --output=./bin/classes.dex ./obj
 
 # put everything in an APK
 echo "##### put everything in an APK"
-aapt package -f -m -F ./bin/unaligned.apk -M ./AndroidManifest.xml -S ./res -I /opt/android-sdk/platforms/android-31/android.jar
+aapt package -f -m -F ./bin/unaligned.apk -M ./AndroidManifest.xml -S ./res -I ${android_jar}android.jar
 cd ./bin
 aapt add unaligned.apk classes.dex
 cd ..
